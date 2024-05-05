631369f078e0d481194752eafa01adc161e04262
Cameron Smith <cameron.ray.smith@gmail.com>
2024-05-05 10:43:21 -0400

    feat(flake): initialize nix flake Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "A development environment for Crossplane Kubernetes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # https://github.com/nix-systems/default/blob/main/default.nix
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs = {
+        systems.follows = "systems";
+      };
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        pkgs_unstable = import inputs.nixpkgs-unstable {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs_unstable; [
+              bat
+              gum
+              gh
+              just
+              kind
+              kubectl
+              yq-go
+              jq
+              (
+                google-cloud-sdk.withExtraComponents
+                [google-cloud-sdk.components.gke-gcloud-auth-plugin]
+              )
+              awscli2
+              azure-cli
+              upbound
+              teller
+              timoni
+              crossplane-cli
+              kubernetes-helm
+              kuttl
+              kyverno-chainsaw
+              watchexec
+            ];
+          };
+        };
+      }
+    );
+}
